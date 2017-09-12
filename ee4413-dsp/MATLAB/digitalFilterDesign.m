bdd2e037e6cb8633f93ea11bfabadfa3aba9e1ca
Vanonymous <seanhxx42@live.com>
2017-09-12 12:41:44 +0800

    Q7.6/7

--- ee4413-dsp/MATLAB/digitalFilterDesign.m
+++ ee4413-dsp/MATLAB/digitalFilterDesign.m
@@ -1,16 +1,16 @@
-% ------------------------Q7.1
+% -------------------------Q7.1/Q7.6
 % fs=40000;
 % Wp=4000/fs*2;
 % Ws=8000/fs*2;
 % Rp=0.5;
 % Rs=40;
 
-% -------------------------Q7.2
-% fs=3500;
-% Wp=1050/fs*2;
-% Ws=600/fs*2;
-% Rp=1;
-% Rs=50;
+% -------------------------Q7.2/Q7.7
+fs=3500;
+Wp=1050/fs*2;
+Ws=600/fs*2;
+Rp=1;
+Rs=50;
 
 % -------------------------Q7.3
 % fs=7000;
@@ -32,31 +32,39 @@
 % [n4,Wn4]=ellipord(Wp,Ws,Rp,Rs)
 
 % -------------------------Q7.5
+% Wp=[0.3 0.7];
+% Ws=[0.4 0.6];
+% Rp=0.4;
+% Rs=50;
+% 
+% [n1, Wn1]=buttord(Wp,Ws,Rp,Rs);
+% [num,den]=butter(n1,Wn1,'stop');
 
-Wp=[0.3 0.7];
-Ws=[0.4 0.6];
-Rp=0.4;
-Rs=50;
+% -------------------------Q7.6
+% [n2,Wn2]=cheb1ord(Wp,Ws,Rp,Rs);
+% [num,den]=cheby1(n2,Rp,Wp);
 
-[n1, Wn1]=buttord(Wp,Ws,Rp,Rs);
-[num,den]=butter(n1,Wn1,'stop');
+% -------------------------Q7.7
+[n3,Wn3]=cheb2ord(Wp,Ws,Rp,Rs);
+[num,den]=cheby1(n2,Rs,Ws,'high');
+
 disp('Numerator coefficients are: ');disp(num);
 disp('Denominator coefficients are: ');disp(den);
 
 [g,w,p]=gain(num,den);
 
-% plot(w/pi,g);grid 
-% axis([0 1 -60 5]);
-% xlabel('\omega /\pi');
-% ylabel('Gain, dB');
-% title('Gain Response of a Butterworth Bandstop Filter');
-
-disp(p);
-plot(w/pi,p);grid
-axis([0 1 -10*pi 0.5*pi]);
+plot(w/pi,g);grid 
+axis([0 1 -60 5]);
 xlabel('\omega /\pi');
-ylabel('Phase, radians');
-title('Phase Response');
+ylabel('Gain, dB');
+title('Gain Response of a Butterworth Bandstop Filter');
+
+% disp(p);
+% plot(w/pi,p);grid
+% axis([0 1 -10*pi 0.5*pi]);
+% xlabel('\omega /\pi');
+% ylabel('Phase, radians');
+% title('Phase Response');
 
 
 
