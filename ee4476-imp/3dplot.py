4b89f0a3b9e6016f28177aefeeb4f1393a5814dd
Vanonymous <seanhxx42@live.com>
2017-09-13 16:56:17 +0800

    3d

--- ee4476-imp/3dplot.py
+++ ee4476-imp/3dplot.py
@@ -7,14 +7,22 @@ import numpy as np
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-X = np.arange(-10, 10, 1)
-Y = np.arange(-10, 10, 1)
+X = np.arange(-5, 5, 0.1)
+Y = np.arange(-5, 5, 0.1)
 X, Y = np.meshgrid(X, Y)
 Z = np.sin(X + Y) + 1
 
 surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
-ax.set_zlim(-3,3)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.
+ax.set_xlim(-5,5)
+ax.set_ylim(-5,5)
+ax.set_zlim(-0.5,2.5)
+ax.xaxis.set_major_locator(LinearLocator(5))
+ax.yaxis.set_major_locator(LinearLocator(5))
+ax.zaxis.set_major_locator(LinearLocator(7))
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+
+plt.show()
