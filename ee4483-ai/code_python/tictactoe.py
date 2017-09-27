fb5d76f8bad0875386c8ee06e364320c6ab4575b
Vanonymous <seanhxx42@live.com>
2017-09-27 11:58:14 +0800

    alpha-beta pruning

--- ee4483-ai/code_python/tictactoe.py
+++ ee4483-ai/code_python/tictactoe.py
@@ -1,8 +1,3 @@
-# cord = [int(i) for i in input("Point:").strip().split(',')]
-# init_gameboard = [[0,0,0],
-#                   [0,0,0],
-#                   [0,0,0]]
-#
 def writeBoard(gameboard):
     gameGraph = []
     for i in range(3):
@@ -16,6 +11,7 @@ def writeBoard(gameboard):
                 _line += "O"
         gameGraph.append(_line)
     return gameGraph
+
 
 def readBoard(input_line):
     line = list(input_line)
@@ -33,36 +29,30 @@ player = input()
 currentBoard = [readBoard(input().strip()) for i in range(3)]
 
 
-def strategyAnalysis(_currentBoard, _currentPlayer, _depth):
+def strategyAnalysis(_currentBoard, _currentPlayer, _depth, previous_aorb):
 
     def evalCurrentBoard(_cB):
 
         def evalLine(p1,p2,p3):
             scores = 0
-            if p1 == 0 and p2 == 0 and p3 == 0: # Empty line [0,0,0] is 5 scores cost
-                # if _currentPlayer == "X":
-                #     scores = -5
-                # elif _currentPlayer == "O":
-                #     scores = 5
+            if p1+p2+p3 == 0:
                 scores = 0
-            # elif p1+p2+p3 == 0: # Line with both players' pieces Ep. [1,-1,0]
-            #     scores = 0
             elif p1+p2+p3 == 3:
-                scores = 1000
+                scores = 2000
             elif p1+p2+p3 == 2:
-                scores = 200
+                scores = 100
             elif p1+p2+p3 == 1:
                 if abs(p1)+abs(p2)+abs(p3) == 1:
-                    scores = 50
+                    scores = 10
                 elif abs(p1)+abs(p2)+abs(p3) == 3:
                     scores = 0
             elif p1+p2+p3 == -3:
-                scores = -1000
+                scores = -2000
             elif p1+p2+p3 == -2:
-                scores = -200
+                scores = -100
             elif p1+p2+p3 == -1:
                 if abs(p1)+abs(p2)+abs(p3) == 1:
-                    scores = -50
+                    scores = -10
                 elif abs(p1)+abs(p2)+abs(p3) == 3:
                     scores = 0
             return scores
@@ -84,62 +74,72 @@ def strategyAnalysis(_currentBoard, _currentPlayer, _depth):
             tempBoard[step[0]][step[1]] = -1
         return tempBoard
 
+    def checkWin():
+        if abs(evalCurrentBoard(_currentBoard)) > 1000:
+            return True
+        else:
+            return False
+
     nextPossibleMoves = []
     for r in range(3):
         for c in range(3):
             if _currentBoard[r][c] == 0:
                 nextPossibleMoves.append([r,c])
 
-    optimized_move = None
-    if _currentPlayer == "X":
-        optimized_scores = -1000
-    elif _currentPlayer == "O":
-        optimized_scores = 1000
-
-    if nextPossibleMoves == [] or _depth == -1:
-        return [[],0]
+    if nextPossibleMoves == [] or _depth == -1 or checkWin():
+        return [[], evalCurrentBoard(_currentBoard), previous_aorb]
     else:
-        for step_move in nextPossibleMoves:
-            newBoard = generateNewBoard(step_move)
-            single_step_move_scores = evalCurrentBoard(newBoard)
+        optimized_move = None
+        if _currentPlayer == "X":
+            _alpha = -10000  # Set local alpha for current Max and pass to next Min
+            _beta = previous_aorb  # Assign previous Min to current beta
+            for current_move in nextPossibleMoves:
+                # Only the second returned value is used as the current_move_score
+                current_move_score = strategyAnalysis(generateNewBoard(current_move), "O", _depth-1, _alpha)[1]
+                if _alpha < current_move_score:
+                    _alpha = current_move_score
+                    optimized_move = current_move
+                    if _alpha > _beta:
+                        break
+            return [optimized_move, _alpha, _beta]
+        elif _currentPlayer == "O":
+            _beta = 10000  # Set local beta for current Min and pass to next Max
+            _alpha = previous_aorb  # Assign previous Max to current alpha
+            for current_move in nextPossibleMoves:
+                # Only the second returned value is used as the current_move_score
+                current_move_score = strategyAnalysis(generateNewBoard(current_move), "X", _depth-1, _beta)[1]
+                if _beta > current_move_score:
+                    _beta = current_move_score
+                    optimized_move = current_move
+                    if _alpha > _beta:
+                        break
+            return [optimized_move, _beta, _alpha]
 
-            print(newBoard)
-            print(single_step_move_scores)
-            if _currentPlayer == "X":
-                total_step_move_scores = single_step_move_scores + \
-                                         strategyAnalysis(newBoard, "O", _depth - 1)[1]
-                if total_step_move_scores > optimized_scores:
-                    optimized_scores = total_step_move_scores
-                    optimized_move = step_move
-            elif _currentPlayer == "O":
-                total_step_move_scores = single_step_move_scores + \
-                                         strategyAnalysis(newBoard, "X", _depth - 1)[1]
-                if total_step_move_scores < optimized_scores:
-                    optimized_scores = total_step_move_scores
-                    optimized_move = step_move
 
-        return [optimized_move,optimized_scores]
-
-def play(_cBoard,_cPlayer,_turns):
+def play(_cBoard, _cPlayer, _turns):
     print("Turn {}".format(_turns))
-    nextStrategy = strategyAnalysis(_cBoard, _cPlayer, 1)
-    nextStep = nextStrategy[0]
-    print(nextStrategy)
-    if nextStep:
-        if _cPlayer == "X":
+    if _cPlayer == "X":
+        nextStrategy = strategyAnalysis(_cBoard, _cPlayer, 2, 10000)
+        print(nextStrategy)
+        nextStep = nextStrategy[0]
+        if nextStep:
             _cBoard[nextStep[0]][nextStep[1]] = 1
-        elif _cPlayer == "O":
-            _cBoard[nextStep[0]][nextStep[1]] = -1
-
-        for i in writeBoard(_cBoard):
-            print(i)
-
-        if _cPlayer == "X":
-            _cPlayer = "O"
+            for i in writeBoard(_cBoard):
+                print(i)
+            return play(_cBoard, "O", _turns+1)
         else:
-            _cPlayer = "X"
-        return play(_cBoard, _cPlayer,_turns+1)
-    else:
-        return print("Game Over!")
+            return print("Game Over!")
+    elif _cPlayer == "O":
+        nextStrategy = strategyAnalysis(_cBoard, _cPlayer, 2, -10000)
+        print(nextStrategy)
+        nextStep = nextStrategy[0]
+        if nextStep:
+            _cBoard[nextStep[0]][nextStep[1]] = -1
+            for i in writeBoard(_cBoard):
+                print(i)
+            return play(_cBoard, "X", _turns+1)
+        else:
+            return print("Game Over!")
+
 
 play(currentBoard,player,1)
