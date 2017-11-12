6a0d24ea719f28499c343acf0cd55be15c50e270
Vanonymous <seanhxx42@live.com>
2017-11-13 00:27:56 +0800

    new

---
+++ ee4483-ai/data_mining_assign/csv2basket.py
@@ -0,0 +1,26 @@
+import re
+import os
+import Orange
+
+in_path = os.path.join('dataset.csv')
+with open(in_path) as f:
+    s = f.read() + '\n'
+
+word = re.compile("\w+")
+all_items = set(word.findall(s))
+
+domain = Orange.data.Domain([])
+domain.add_metas({Orange.orange.newmetaid(): Orange.feature.Continuous(n)
+                  for n in all_items}, True)
+
+data = Orange.data.Table(domain)
+for e in s.splitlines():
+    ex = Orange.data.Instance(domain)
+    for m in re.findall("\w+", e):
+        ex[m] = 1
+    data.append(ex)
+
+out_path = 'dataset.basket'
+f = open(out_path, 'wb')
+f.write(data)
+f.close()
--- ee4483-ai/data_mining_assign/csv2tab.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-csv_file = 'dataset.csv'
-output = 'dataset.tab'
-
-in_file = csv.reader(open(csv_file, 'r'))
-out_file = csv.writer(open(output, 'w'), delimiter='\t')
-
-out_file.writerows(in_file)
